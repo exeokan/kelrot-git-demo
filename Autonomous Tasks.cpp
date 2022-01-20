07279d6236e6653e5788fc8c02c85d5ab3b3a36b
exe_okan <egeokanunaldi@hotmail.com>
2022-01-20 17:49:11 +0300

    ultrasonic command

--- Autonomous Tasks.cpp
+++ Autonomous Tasks.cpp
@@ -1,5 +1,6 @@
 void DriveNearObject(Ultrasonic *us,DriveBase *drive,dobule stopDistance){
-
+    if(us.dist<10)
+        drive.drive(1,0);
 }
 void DriveDistance(Encoder *enc, DriveBase *drive,double dist, double kP){
 
