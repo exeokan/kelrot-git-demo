c5f4a9c5336ac9c8313d3fe4fcea40f279764350
exe_okan <egeokanunaldi@hotmail.com>
2022-01-20 17:53:51 +0300

    enc func

--- Autonomous Tasks.cpp
+++ Autonomous Tasks.cpp
@@ -1,7 +1,9 @@
 void DriveNearObject(Ultrasonic *us,DriveBase *drive,dobule stopDistance){
-    if(us.dist<10)
+    if(us.dist()>10)
         drive.drive(1,0);
+    else
+        drive.drive(0,0);
 }
-void DriveDistance(Encoder *enc, DriveBase *drive,double dist, double kP){
-
+void DriveDistance(Encoder *enc, DriveBase *drive,double setpoint, double kP){
+    drive.drive((setpoint-enc.get())*kP,0);
 }
